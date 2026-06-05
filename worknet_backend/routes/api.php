b24204dbb1929ec6605237a57e9e043fb93c471e
conectividadatusmanos <conectividadatusmanos@gmail.com>
2026-06-04 23:13:12 -0500

    insertar solo datos en tabla usuarios

--- worknet_backend/routes/api.php
+++ worknet_backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\PerfilesCandidatosController;
 use Illuminate\Support\Facades\Route;
 
 // Ruta para listar (Metodo GET)
@@ -7,3 +8,9 @@ Route::get('/usuarios', [UsuariosController::class, 'index']);
 
 // Ruta para registrar desde tu formulario (Metodo POST) -> Esta es la que usa tu Axios
 Route::post('/usuarios', [UsuariosController::class, 'store']);
+
+// Ruta para listar (Metodo GET)
+Route::get('/perfiles_candidatos', [PerfilesCandidatosController::class, 'index']);
+
+// Ruta para registrar desde tu formulario (Metodo POST) -> Esta es la que usa tu Axios
+Route::post('/perfiles_candidatos', [PerfilesCandidatosController::class, 'store']);
